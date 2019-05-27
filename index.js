e63627c6d9bb3407af2e624da3017a667a3b0629
Christoph Gruener <christophgruener@me.com>
2019-05-27 16:55:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers){
   let newDrivers = drivers.map(function(drvr){
-    return Object.assign({}, drvr, {})
+    return drvr.name.toLowerCase();
   })
 }
